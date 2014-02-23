8b2350a983792ef0a7282be49b1444162c899e37
王薇薇 <guodonghai901901@sina.cn>
2014-02-24 00:42:05 +0800

    Signed-off-by: 王薇薇 <guodonghai901901@sina.cn>

---
+++ ex4+.py
@@ -0,0 +1,8 @@
+#Traceback (most recent call last):
+#File "ex4.py", line 8, in <module>
+#average_passengers_per_car = car_pool_capacity / passenger
+#NameError: name 'car_pool_capacity' is not defined
+#解释：文件ex4.py在第8行有错误，变量car_pool_capacity没有定义
+
+#1. 会输出120整数型值。
+#2. 带有小数点的类型
--- ex4.py
+++ ex4.py
@@ -1,1 +1,22 @@
-p
+#给变量cars赋值100整数
+cars = 100
+#给变量space_in_a_car赋值4.0浮点数
+space_in_a_car = 4.0
+#给变量drivers赋值30整数
+drivers = 30
+#给变量passengers赋值90整数
+passengers = 90
+#变量cars_not_driven被赋值为cars变量减去drivers变量的值
+cars_not_driven = cars - drivers
+#变量cars_driven被赋值为drivers
+cars_driven = drivers
+#carpool_capacity被赋值为变量cars_driven与space_in_a_car的乘数值
+carpool_capacity = cars_driven * space_in_a_car
+#average_passengers_per_car被赋值为变量passgeners与变量cars_driven相除后的值
+average_passengers_per_car = passengers / cars_driven
+print "There are", cars, "cars available." 
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
