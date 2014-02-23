b2aac58b4f8421aa859663f0067758c511ff62ea
王薇薇 <guodonghai901901@sina.cn>
2014-02-23 22:56:25 +0800

    Signed-off-by: 王薇è? <guodonghai901901@sina.cn>

---
+++ ex3+.py
@@ -0,0 +1,8 @@
+print 1%1
+print 1/1
+print 1-1
+print 1+2
+print 1*8*7
+print 7.0/4.0
+print 7/4
+print 7%4
---
+++ ex3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#这行是为了输入一般字符串.
+print "I will now count my chickens:"
+#这行是为了区别字符串和数值
+print "Hens", 25 + 30 / 6
+#这行是为了区别字符串和特别运算，尽管我还不是很理解
+print "Roosters", 100 -  25 * 3 % 4
+#这行是为了再次输入一般字符串。
+print "Now i will count the eggs:"
+#这行是为了各种运算
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#这行是看运算符放入字符串中会否正常计算后输出
+print "Is it true that 3 + 2 < 5 - 7?"
+#这行是对比上面一行看是否正常计算后输出
+print 3 + 2 < 5 - 7
+#这行是在一个输出里面对比在字符串中的加号运算符和字符串外的加号运算符
+print "What is 3 + 2?", 3 + 2
+#这行是为了区别在字符串中的减号和在字符串外的减号的输出情况
+print "What is 5 - 7?", 5 - 7
+#这行是输出字符串
+print "Oh, that's why it's False."
+#这行继续输出字符串
+print "How about some more."
+#这行是输出字符串和输出逻辑值
+print "Is it greater?", 5 > -2
+#这行是输出字符串和输出逻辑值
+print "Is it greater or equal?", 5 >= -2
+#这行是输出字符串和输出逻辑值
+print "Is it less or equal?", 5 <= -2
